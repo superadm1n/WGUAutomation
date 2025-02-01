4143d9450305d62a9f927d5bd91f9039691a3413
Kyle Kowalczyk <kowalkyl@gmail.com>
2025-02-01 12:36:43 -0600

    na

--- pt1-c2.py
+++ pt1-c2.py
@@ -18,9 +18,11 @@ if __name__ == '__main__':
 
     config = read_yaml('ansible_inventory.yaml')
 
+    vlan_id = 10
     for ip, data in config.get('switches', {}).items():
         print(f'=== {ip} | {data.get("General settings", "").get("name", "")} ===')
         with ConnectHandler(device_type='extreme_exos', host=ip, username=data.get('vars', {}).get('ansible_user', {}), password='') as ssh:
-            print(ssh.send_command(f'create vlan 10 description {data.get("General settings", "").get("name", "")}'))
-            print(ssh.send_command(f'configure vlan 10 name {data.get("General settings", "").get("name", "")}'))
+            print(ssh.send_command(f'create vlan {vlan_id} description {data.get("General settings", "").get("name", "")}'))
+            print(ssh.send_command(f'configure vlan {vlan_id} name {data.get("General settings", "").get("name", "")}'))
         print(f'\n{"="*10}\n')
+        vlan_id += 1
