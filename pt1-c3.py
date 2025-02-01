8bb9129389940ff495acc1a7fda2a773d7045463
Kyle Kowalczyk <kowalkyl@gmail.com>
2025-02-01 12:53:11 -0600

    na

---
+++ pt1-c3.py
@@ -0,0 +1,25 @@
+"""
+Part 1 Task C Objective 3
+"""
+import yaml
+from netmiko import ConnectHandler
+
+
+def read_yaml(file):
+    """Function to read YAML file"""
+    with open(file) as stream:
+        try:
+            return yaml.safe_load(stream)
+        except Exception as e:
+            raise e
+
+
+if __name__ == '__main__':
+
+    config = read_yaml('ansible_inventory.yaml')
+
+    for ip, data in config.get('switches', {}).items():
+        print(f'=== {ip} | {data.get("General settings", "").get("name", "")} ===')
+        with ConnectHandler(device_type='extreme_exos', host=ip, username=data.get('vars', {}).get('ansible_user', {}), password='') as ssh:
+            print(ssh.send_command('show vlan'))
+        print(f'\n{"="*10}\n')
