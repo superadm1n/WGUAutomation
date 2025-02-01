80c7024aa279c6fdb518d5abeb8a2f0c26cd4383
Kyle Kowalczyk <kowalkyl@gmail.com>
2025-02-01 11:45:05 -0600

    initial commit

---
+++ inventory_generate.py
@@ -0,0 +1,30 @@
+import yaml
+
+
+def read_yaml(file):
+    """Function to read YAML file"""
+    with open(file) as stream:
+        try:
+            return yaml.safe_load(stream)
+        except Exception as e:
+            raise e
+
+
+def write_yaml(file, data):
+    """Function to write YAML file"""
+    with open(file, 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+
+# Read in the base YAML file
+gns_data = read_yaml('gns_data.yaml')
+
+# add ansible information to each switch
+for ip, data in gns_data.items():
+    data['vars'] = {
+        'ansible_user': 'admin',
+        'ansible_connection': 'ansible.netcommon.network_cli',
+        'ansible_network_os': 'exos'
+    }
+
+# save new file
+write_yaml('ansible_inventory.yaml', gns_data)
