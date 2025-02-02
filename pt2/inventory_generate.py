a1c888338c5e3653126b5101d59a6f7faae251d6
Kyle Kowalczyk <kowalkyl@gmail.com>
2025-02-01 18:21:55 -0600

    na

--- pt2/inventory_generate.py
+++ pt2/inventory_generate.py
@@ -21,7 +21,6 @@ gns_data = read_yaml('gns_data.yaml')
 # add ansible information for each linux host
 gns_data.get('linux', {})['vars'] = {
         'ansible_password': 'P@ssw0rd',
-        'ansible_become_password': 'P@ssw0rd',
     }
 # add ansible information to each windows device
 gns_data.get('windows', {})['vars'] = {
